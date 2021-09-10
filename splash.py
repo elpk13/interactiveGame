9bf6e0ac6b8879b67bebd02373b34e0ea44b7625
elpk13 <eaperkinson@gmail.com>
2021-09-10 11:27:31 -0400

    first version file

---
+++ splash.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game")
+WHITE = (255,255,255)
+FPS = 60
+WOLF_GREY_IMAGE = pygame.image.load(os.path.join('Assets', "greyWolf.png"))
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(WOLF_GREY_IMAGE, (450,250))
+    pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
