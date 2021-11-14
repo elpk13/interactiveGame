a3c1d64badb00d7e0418c8a965aef0c292116210
RMRattray <89876227+RMRattray@users.noreply.github.com>
2021-11-14 18:52:03 -0500

    New methods
    Effectively replaces gameplay_methods.py for use in chapter 1.

---
+++ classes.py
@@ -0,0 +1,158 @@
+import math
+
+# Any object that exists in the world is of object class.
+class Object:
+    def __init__(self,xpos,ypos,height,width,dynamic,appearance,nightappearance):
+        self.xpos = xpos # Distance between object's left side and that of the world.
+        self.ypos = ypos # Distance between object's TOP side and that of the world.
+        self.width = width
+        self.height = height
+        self.dynamic = dynamic # A Boolean indicating whether the object is animated.
+        self.appearance = appearance # Could be a Surface or a list or a list of lists of surfaces
+        self.nightappearance = nightappearance
+        self.ybase = ypos + height # Used for sorting blit order.
+
+    def draw(self,screen,playerx,playery,window_width,window_height,night):
+        if night: # Night-time is defined in globals.  Seasons are defined in the tree class.
+            if self.dynamic:
+                appearance = self.nightappearance[len(self.nightappearance)/time]
+            else:
+                appearance = self.nightappearance
+        else:
+            if self.dynamic:
+                appearance = self.appearance[len(self.appearance)/time]
+            else:
+                appearance = self.appearance
+        screen.blit(appearance,(int(self.xpos-playerx+window_width/2),int(self.ypos-playery+window_height/2)))
+
+# Objects that impede the player's movement have a collision box as well.
+# The 'collidesat' checks if a point is inside it.
+# A collision box is - in this code - a tuple (a,b,c,d), 0 < a < b < 1,
+# 0 < c < d < 1, referring to the portion of the image considered the box.
+class Obstacle(Object):
+    def __init__(self,xpos,ypos,height,width,dynamic,appearance,nightappearance,collisionBox):
+        self.collisionBox = collisionBox
+        super().__init__(xpos,ypos,height,width,dynamic,appearance,nightappearance)
+
+    def collidesat(self,possiblex,possibley):
+        if self.collisionBox[0] < (possiblex-self.xpos)/self.width < self.collisionBox[1] and self.collisionBox[2] < (possibley-self.ypos)/self.height < self.collisionBox[3]:
+            return True
+        return False
+
+# Objects below have specific appearances that should relate to other properties,
+# like a stream's aim or a tree's type; however, these are passed to the class in
+# the initialization functions like 'pourStream' or 'plantTree' so that the
+# graphics dictionaries can be passed as well.
+
+class StreamSegment(Object): # appearance should come from the getStreamGraphics dictionary
+    def __init__(self,xpos,ypos,height,width,dynamic,appearance,nightappearance,aim):
+        self.aim = aim
+        super().__init__(xpos,ypos,height,width,dynamic,appearance,nightappearance)
+
+    def __str__(self):
+        return 'A stream segment of aim ' + self.aim + ' at (' + self.xpos + ',' + self.ypos + ')'
+
+    def covers(self,possiblex,possibley,streamCurveCoefficients): # Curve coefficients come from
+        x = possiblex - self.xpos                                 # the dictionary.
+        y = self.ypos + self.height - possibley
+        if not (0 < x < self.width and 0 < y < self.height):
+            return False
+        if self.aim in ['30','45','60']:
+            if y > x*math.tan(int(self.aim)) and y < x*math.tan(int(self.aim)) + 50/math.cos(int(self.aim)):
+                return True
+            return False
+        elif self.aim[-1] == 's':
+            if ((x-self.width/2)**2+(y-self.height/2)**2)**0.5 < self.width/2:
+                return True
+            elif x < self.width/2 and y > x*math.tan(int(self.aim[:1])) and y < x*math.tan(int(self.aim[:1])) + 50/math.cos(int(self.aim[:1])):
+                return True
+            return False
+        else:
+            f, g = streamCurveCoefficients[self.aim]
+            if f[0]*x**3 + f[1]*x**2 + f[2]*x + f[3] > y > g[0]*x**3 + g[1]*x**2 + g[2]*x + g[3]:
+                return True
+            return False
+
+class Tree(Obstacle): # The 'appearance' and 'evergreen' states should come from the getTreeGraphics dictionary.
+    def __init__(self,xpos,ypos,height,width,dynamic,appearance,nightappearance,type,evergreen):
+        self.type = type
+        self.evergreen = evergreen # Collision box for trees is defined here \|/
+        super().__init__(xpos,ypos,height,width,dynamic,appearance,nightappearance,(0.4,0.6,0.6,0.8))
+
+    def __str__(self):
+        return 'A(n) ' + self.type + ' tree at (' + self.xpos + ',' + self.ypos + ')'
+
+    def draw(self,screen,playerx,playery,window_width,window_height,time,night):
+        if time % 2400 < 1200:
+            s = 0 # And the leaves that are green...
+        elif time % 2400 < 1800:
+            s = 1 # turn to brown
+        else:
+            s = 2 # and they wither with the wind
+        m = 3
+        t = time % (8*m)
+        if t % (4*m) < 2*m: # Unique animation for trees is described in a
+            f = m # side document.
+        elif t < 3*m:
+            f = t - m
+        elif t < 4*m:
+            f = 5*m - t
+        elif t < 7*m:
+            f = 7*m - t
+        else:
+            f = t-7*m
+        if night:
+            if self.evergreen:
+                appearance = self.nightappearance[f]
+            else:
+                appearance = self.nightappearance[s][f]
+        else:
+            if self.evergreen:
+                appearance = self.appearance[f]
+            else:
+                appearance = self.appearance[s][f]
+        screen.blit(appearance,(int(self.xpos-playerx+window_width/2),int(self.ypos-playery+window_height/2)))
+
+class Rock(Obstacle): # Appearance can be any Surface.  Ideally one depicting a rock.
+    def __init__(self,xpos,ypos,height,width,appearance,nightappearance):
+        super().__init__(xpos,ypos,height,width,False,appearance,nightappearance,(0.2,0.8,0.2,0.8))
+
+class Decoration(Object): # Not necessary, but seemed nice
+    def __init__(self,xpos,ypos,height,width,dynamic,appearance,nightappearance):
+        super().__init__(xpos,ypos,height,width,dynamic,appearance,nightappearance)
+
+class Interactive(Object):
+    def __init__(self,xpos,ypos,height,width,dynamic,appearance,nightappearance):
+        super().__init__(xpos,ypos,height,width,dynamic,appearance,nightappearance)
+
+    def covers(self,possiblex,possibley):
+        if self.xpos < possiblex < self.xpos + self.width and self.ypos < possibley < self.ypos + self.height:
+            return True
+        return False
+
+class Print(Interactive): # Apperance should come from dictionary
+    def __init__(self,xpos,ypos,height,width,animal,appearance):
+        self.animal = animal
+        super().__init__(xpos,ypos,height,width,False,appearance,appearance)
+
+class Wolf: # Consider making part of a general animal class for the hunting game?
+    def __init__(self,name,framelists):
+        self.name = name
+        self.framelists = framelists
+
+class World:
+    def __init__(self,worldx,worldy,background,nightbackground,streams,forest,rocks,prints,decorations,settlements):
+        self.width = worldx # Dimensions of the world
+        self.height = worldy
+        self.background = background # Background, which should be scaled to the dimensions of the world.
+        self.nightbackground = nightbackground # in its initializer, generateWorld().
+        self.streams = streams # A list of lists of stream segments
+        self.forest = forest # A list of trees
+        self.rocks = rocks # A list of rocks (really, any obstacles)
+        self.prints = prints # A list of prints
+        self.decorations = decorations # A list of decoration objects
+        self.settlements = settlements # A list of settlement objects
+        self.obstacles = forest + rocks # All obstacles, to be sent to the posok() function
+        self.interactives = prints + settlements # All interactives, to be sent to the collision() function
+        self.objectsofheight = forest + rocks + prints + decorations + settlements
+        self.objectsofheight.sort(key=lambda x:x.ybase) # Sort all blittables, to be sent to the drawscreen() function
---
+++ worldgeneration.py
@@ -0,0 +1,266 @@
+from classes import *
+import pygame
+import random
+import os
+import math
+import dialog
+import bisect
+
+# This script contains two functions:  getWorldGraphics, which loads all the
+# images that appear in the world from their files and sends them in dictionaries.
+# Running this function only once allows the image loading to only occur once,
+# good for keeping world generation fast.  Keeping all image loading in this
+# function also makes it a one-stop place to check filenames, though menu images
+# are not a part of it.
+
+# The second function, generateWorld, produces an object of the class World,
+# which has as attributes lists of every object to appear in the world.
+# This function has two primary parts:  the first, a set of sub-functions that
+# initialize objects of our own classes (e.g., plantTree), the second, a set of
+# subfunctions that produces lists of these to be sent to the world object.
+
+# Argument names in subfunctions generally match those in the parent function.
+
+def getWorldGraphics(window_height,worldx=0,worldy=0,bgname="Map_Background.png"):
+    # worldx = worldy = 0 causes them to be determined by the dimensions of the background image.
+    def getBackground(bgname,worldx=0,worldy=0):
+        background = pygame.image.load(os.path.join('Assets',"Map_Background.png"))
+        nightbackground = pygame.image.load(os.path.join('Assets',"Map_Background_Night.png"))
+        if worldx + worldy > 0:
+            background = pygame.transform.scale(background,(worldx,worldy))
+            nightbackground = pygame.transform.scale(nightbackground,(worldx,worldy))
+        return background, nightbackground
+
+    background, nightbackground = getBackground(bgname,worldx,worldy)
+    worldx = background.get_width()
+    worldy = background.get_height()
+
+    def getCharacter(height): # Returns list of framelists for four animations and name.
+        # Identity of the character determined from settings file edited by choice screen,
+        # not passed to function.
+        settingsfile = open("settings.txt","r") # Retrieve current status of settings
+        currentsettings = settingsfile.readlines() # from settings file.
+        charid = int(currentsettings[2][0])
+        settingsfile.close() # Choose character and name.
+        charactername = ['Aspen','Khewa','Mani','Nico','Sparrow','Timber'][charid]
+
+        pframers = [] # Form list of animations.  One for right-walking, one for
+        pframels = [] # left-walking, one up, one down.
+        pframeus = [] # Each starts with standing and loops from the third frame.
+        pframeds = []
+        for f in range(1,9):
+            frame = pygame.image.load(os.path.join('Animations','Wolves',charactername,charactername + '_Walking_Right000' + str(f) + '.png'))
+            frame = pygame.transform.scale(frame,(int(height*frame.get_width()/(9*frame.get_height())),int(height/9)))
+            pframers.append(frame)
+            frame = pygame.transform.flip(frame,True,False)
+            pframels.append(frame)
+            frame = pygame.image.load(os.path.join('Animations','Wolves',charactername,charactername + '_Walking_Forward000' + str(f) + '.png'))
+            frame = pygame.transform.scale(frame,(int(height*frame.get_width()/(9*frame.get_height())),int(height/9)))
+            pframeus.append(frame)
+            frame = pygame.image.load(os.path.join('Animations','Wolves',charactername,charactername + '_walking_Away000' + str(f) + '.png'))
+            frame = pygame.transform.scale(frame,(int(height*frame.get_width()/(7*frame.get_height())),int(height/7)))
+            pframeds.append(frame)
+
+        if charactername == "Mani":
+            charactername = "Máni"  # Accent mark not in filename.
+
+        return Wolf(charactername,[pframers,pframels,pframeus,pframeds])
+
+    character = getCharacter(window_height)
+
+    def getStreamGraphics():
+        streamAppearancesByAim = { } # The 'aim' of a stream roughly refers to the
+        streamNightAppearancesByAim = { } # direction south of the west-axis in
+        appearances = [] # which it flows.  Rivers flow south-west, always.
+        nightappearances = []
+        streamDimensionsByAim = { } # 's' refers to source.
+        for aim in ['30','45','60','30s','45s','60s','30-45','45-60','60-30','30-60','60-45','45-30']:
+            for i in range(1,4):  # Change length of stream animations here.
+                appearances.append(pygame.image.load(os.path.join('Animations','Streams',aim+'000'+str(i)+'.png')))
+                nightappearances.append(pygame.image.load(os.path.join('Animations','Streams',aim+'000'+str(i)+'_Night.png')))
+            streamAppearancesByAim[aim] = appearances
+            streamNightAppearancesByAim[aim] = nightappearances
+            width, height = appearances[0].get_width(), appearances[0].get_height()
+            streamDimensionsByAim[aim] = (width,height)
+            appearances = []
+            nightappearances = []
+        return streamAppearancesByAim, streamNightAppearancesByAim, streamDimensionsByAim
+
+    streamAppearancesByAim, streamNightAppearancesByAim, streamDimensionsByAim = getStreamGraphics()
+
+    def getStreamCurveCoefficients(): # The coefficients of curves in stream bends
+        def fg(t,w,i,e): # need only be calculated once.  This method is explained in a side document.
+            def r(d): # Cool, a fourth-order nested function!
+                return int(d)*math.pi/180 # Converts degree-strings to radians.
+            f = ( (t*math.tan(r(i))+t*math.tan(r(e))+2*w/math.cos(r(e))-2*t)/t**3 , (3*t-3*w/math.cos(r(e))-2*t*math.tan(r(e))-t*math.tan(r(i)))/t**2 , math.tan(r(e)) , w/math.cos(r(e)) )
+            g = ( (t*math.tan(r(i))+t*math.tan(r(e))+2*w/math.cos(r(i))-2*t)/t**3 , (3*t-3*w/math.cos(r(e))-2*t*math.tan(r(e))-t*math.tan(r(i)))/t**2 , math.tan(r(e)) , 0 )
+            return f, g
+        streamCurveCoefficients = { }
+        for aim in ['30-45','30-60','45-30','45-60','60-30','60-45']:
+            streamCurveCoefficients[aim] = fg(300,50,aim[:1],aim[-2:])
+        return streamCurveCoefficients
+
+    streamCurveCoefficients = getStreamCurveCoefficients()
+
+    def getTreeGraphics():
+        treeGraphics = { } # Dictionary of framelists (one for each season) by type.
+        treeNightGraphics = { }
+        treeGreenness = { } # Dictionary of whether a tree is evergreen (same image in winter)
+        for type in ['White_Oak']: # List all non-evergreen trees here.
+            summerdays = []
+            summernits = []
+            autumndays = []
+            autumnnits = []
+            winterdays = []
+            winternits = []
+            for i in range(1,8):
+                summerdays.append(pygame.image.load(os.path.join('Animations','Trees',type+'_Summer000'+str(i)+'.png')))
+                summernits.append(pygame.image.load(os.path.join('Animations','Trees',type+'_Summer000'+str(i)+'_Night.png')))
+                autumndays.append(pygame.image.load(os.path.join('Animations','Trees',type+'_Autumn000'+str(i)+'.png')))
+                autumnnits.append(pygame.image.load(os.path.join('Animations','Trees',type+'_Autumn000'+str(i)+'_Night.png')))
+                winterdays.append(pygame.image.load(os.path.join('Animations','Trees',type+'_Winter000'+str(i)+'.png')))
+                winternits.append(pygame.image.load(os.path.join('Animations','Trees',type+'_Winter000'+str(i)+'_Night.png')))
+            treeGraphics[type] = (summerdays,autumndays,winterdays)
+            treeNightGraphics[type] = (summernits,autumnnits,winternits)
+            treeGreenness[type] = False
+        for type in ['Spruce']: # List all evergreen trees here.
+            days = []
+            nits = []
+            for i in range(1,8):
+                days.append(pygame.image.load(os.path.join('Animations','Trees',type+'000'+str(i)+'.png')))
+                nits.append(pygame.image.load(os.path.join('Animations','Trees',type+'000'+str(i)+'_Night.png')))
+            treeGraphics[type] = days
+            treeNightGraphics[type] = nits
+            treeGreenness[type] = True
+        return treeGraphics, treeNightGraphics, treeGreenness
+
+    treeGraphics, treeNightGraphics, treeGreenness = getTreeGraphics()
+
+    def getPrintGraphics(height): # Height is the height of the world, by which prints are scaled.
+        printGraphics = { } # Prints, large and identifiable for dialogs
+        printGraphicsSmall = { } # Blitted images in the world
+        animalTypes = { } # Types by animal - 0 for prey, 1 for mutual, 2 for predator
+        for animal in ['bison']: # List predators here - bear, moose
+            printImage = pygame.image.load(os.path.join('Assets',animal+"_print.png"))
+            printImageSmall = pygame.transform.scale(printImage,(int(height/15),int(height/15)))
+            printGraphics[animal] = printImage
+            printGraphicsSmall[animal] = printImageSmall
+            animalTypes[animal] = 2
+        for animal in []: # List neither predator nor prey here - raccoons, foxes
+            printImage = pygame.image.load(os.path.join('Assets',animal+"_print.png"))
+            printImageSmall = pygame.transform.scale(printImage,(int(height/15),int(height/15)))
+            printGraphics[animal] = printImage
+            printGraphicsSmall[animal] = printImageSmall
+            animalTypes[animal] = 1
+        for animal in ['deer','rabbit']: # List prey here
+            printImage = pygame.image.load(os.path.join('Assets',animal+"_print.png"))
+            printImageSmall = pygame.transform.scale(printImage,(int(height/15),int(height/15)))
+            printGraphics[animal] = printImage
+            printGraphicsSmall[animal] = printImageSmall
+            animalTypes[animal] = 0
+        return printGraphics, printGraphicsSmall, animalTypes
+
+    printGraphics, printGraphicsSmall, animalTypes = getPrintGraphics(window_height)
+
+    return worldx, worldy, background, nightbackground, character, streamAppearancesByAim, streamNightAppearancesByAim, streamDimensionsByAim, streamCurveCoefficients, treeGraphics, treeNightGraphics, treeGreenness, printGraphics, printGraphicsSmall, animalTypes
+
+# Observe that not all the output from the above function is input into that
+# below.  The character, the animalTypes dictionary, and the larger print graphics
+# are not necessary for the construction of the world.
+
+def generateWorld(worldx,worldy,background, nightbackground, streamAppearancesByAim, streamNightAppearancesByAim, streamDimensionsByAim, streamCurveCoefficients, treeGraphics, treeNightGraphics, treeGreenness, printGraphicsSmall):
+    # The first part of this function, concerning sub-functions that initialize
+    # user-defined classes.  Updates to classes probably need to be updated here.
+
+    def pourStream(worldx,worldy,streamAppearancesByAim,streamNightAppearancesByAim,streamDimensionsByAim):
+        sourcex = random.randint(0,worldx) # Place a random source, pick a direction, and go.
+        sourcey = random.randint(0,worldy)
+        dir = random.choice(['30','45','60'])
+        riverys = {'30':58,'45':71,'60':100} # Vertical height of the river at crossing, by angle.
+                                             # Equals width times the secant of the angle.
+        aim = dir+'s'
+        stream = [StreamSegment(sourcex,sourcey,200,200,True,streamAppearancesByAim[aim],streamNightAppearancesByAim[aim],aim)]
+        runningx = sourcex
+        runningy = sourcey + 200 - riverys[dir]
+
+        while runningx > 0 and runningy < worldy:
+            if random.random() > 0.25: # Half a chance of changing direction with each.
+                newdir = random.choice(['30','45','60'])
+            else:
+                newdir = dir
+            if newdir == dir:
+                aim = dir
+            else:
+                aim = '-'.join([dir,newdir])
+            segwidth, segheight = streamDimensionsByAim[aim]
+            stream.append(StreamSegment(int(runningx-segwidth),runningy,segwidth,segheight,True,streamAppearancesByAim[aim],streamNightAppearancesByAim[aim],aim))
+            runningx -= segwidth
+            runningy += segheight - riverys[newdir]
+            dir = newdir
+        return stream # Returns list of segments
+
+    def dry(x,y,streams=[]): # This function checks whether any
+        for stream in streams: # streams cover a given point.  Used in later
+            for segment in stream: # initialization functions to ensure that
+                if segment.covers(x,y,streamCurveCoefficients): # trees, etc.
+                    return False                                # not in water.
+        return True
+
+    def plantTree(worldx,worldy,type,streams,treeGraphics,treeNightGraphics,treeGreenness):
+        while True:
+            x = random.randint(0,worldx)
+            y = random.randint(0,worldy)
+            if dry(x,y,streams):
+                appearance = treeGraphics[type]
+                if treeGreenness[type]:
+                    height = appearance[0].get_height()
+                    width = appearance[0].get_width()
+                else:
+                    height = appearance[0][0].get_height()
+                    width = appearance[0][0].get_width()
+                return Tree(int(x-width/2),int(y-3*height/4),height,width,True,treeGraphics[type],treeNightGraphics[type],type,treeGreenness[type])
+
+    def stampPrint(worldx,worldy,animal,streams,printGraphicsSmall):
+        while True:
+            x = random.randint(0,worldx)
+            y = random.randint(0,worldy)
+            if dry(x,y,streams):
+                appearance = printGraphicsSmall[animal]
+                return Print(x,y,appearance.get_height(),appearance.get_width(),animal,appearance)
+
+    # The second part of this function concerns the placement of objects
+    # initialized in the above code, beginning with streams and continuing
+    # through obstacles and decorations.
+
+    mystreams = [] # Pourstream is sufficient; I didn't feel like making a whole subfunction for the multiple streams.
+    for s in range(2//random.randint(1,6)): # Maximum 2, but probably just one.
+        mystreams.append(pourStream(worldx,worldy,streamAppearancesByAim,streamNightAppearancesByAim,streamDimensionsByAim))
+
+    def forestWorld(worldx,worldy,treeTypes,streams,treeGraphics,treeNightGraphics,treeGreenness):
+        treecount = worldx*worldy * random.randint(28,175) // 10000000   # Based on historical forest estimates
+        if len(treeTypes) > 7: # If we get so far, forests can be unique # and an arbitrary conversion of pixels
+            treeTypes = random.sample(treeTypes,random.randint(5,7))     # to real-life units.
+        forest = []
+        for t in range(treecount):
+            forest.append(plantTree(worldx,worldy,random.choice(treeTypes),streams,treeGraphics,treeNightGraphics,treeGreenness))
+        forest.sort(key=lambda x: x.ybase)
+        return forest
+
+    myforest = forestWorld(worldx,worldy,list(treeGraphics),mystreams,treeGraphics,treeNightGraphics,treeGreenness)
+
+    def leavePrints(worldx,worldy,printGraphicsSmall,streams):
+        count = worldx*worldy // 1000000
+        prints = []
+        for i in range(count):
+            prints.append(stampPrint(worldx,worldy,random.choice(list(printGraphicsSmall)),streams,printGraphicsSmall))
+        return prints
+
+    myprints = leavePrints(worldx,worldy,printGraphicsSmall,mystreams)
+
+    return World(worldx,worldy,background,nightbackground,mystreams,myforest, [     ] ,myprints, [         ], [          ])
+#    return World(worldx,worldy,background,nightbackground,mystreams,myforest,myrocks,myprints,mydecorations,mysettlements)
+# When we iterate through a null list, it can only throw syntax errors.  Try:
+# for i in []:
+#     tom = turtle.Turtle()
+#     tom.throwmeanerror(idareyou)
+# in a command prompt; it's fun!
